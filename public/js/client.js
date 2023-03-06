da2e35280a59cf0a375e51c7c46e5b1ab4106403
Justinel2 <justine.lardeux@hotmail.com>
2023-03-06 14:01:26 -0500

    embedded scanner

--- public/js/client.js
+++ public/js/client.js
@@ -1,28 +1,289 @@
 window.onload = function () {
   console.log("we are loaded");
 
- //GET DATA BY UPC CODE
- document.querySelector("#findDataWithKeyword").addEventListener('click', function(event){
-  console.log("click");
-  let keyword = document.getElementById("searchCritKeyword").value;
-  console.log(keyword);
-  $.get(
-    "/searchManualKeyword",
-    {keywordSubmitted : keyword},
-   // if we get a response from the server .... 
-    function(response) {
-       console.log("Mission was aborted:" + response[0]);
-       if (response[0]) {
-        let error = "Sorry, couldn't find anything. Try again."
-        console.log(error);
-        displayErrorMessage(error);
-       }
-       else {
-        displayItem(response[1]);
-        displayCompany(response[2]);
-       }
-  })
-});//click
+  //SCANNING
+  var barcodeDetector;
+  var decoding = false;
+  var localStream;
+  var interval;
+  var scannerContainer = document.querySelector(".scanner");
+  var home = document.querySelector(".home");
+  var startButton = document.querySelector("#startButton");
+  startButton.onclick = function() {
+    scannerContainer.style.display = "";
+    home.style.display = "none";
+    loadDevicesAndPlay();
+  }
+  var fileInput = document.querySelector("#fileInput")
+  fileInput.onchange = function(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+          
+    reader.onload = function(e){
+      var img = document.getElementById("selectedImg");
+      img.src = e.target.result;
+      img.onload = async function() {
+        var detectedCodes = await barcodeDetector.detect(img);
+        var json = JSON.stringify(detectedCodes, null, 2);
+        console.log(json);
+        alert(json);
+      }
+    };
+      
+    reader.onerror = function () {
+      console.warn('oops, something went wrong.');
+    };
+      
+    reader.readAsDataURL(file);	
+  }
+
+  var closeButton = document.querySelector("#closeButton");
+  closeButton.onclick = function() {
+    stop();
+    scannerContainer.style.display = "none";
+    home.style.display = "";
+  }
+  document.getElementsByClassName("camera")[0].addEventListener('loadeddata',onPlayed, false);
+  document.getElementById("cameraSelect").onchange = onCameraChanged;
+  initBarcodeDetector();
+
+
+  async function initBarcodeDetector(){
+    var barcodeDetectorUsable = false;
+    if ('BarcodeDetector' in window) {
+      let formats = await window.BarcodeDetector.getSupportedFormats();
+      if (formats.length > 0) {
+        barcodeDetectorUsable = true;
+      }
+    }
+
+    if (barcodeDetectorUsable === true) {
+      alert('Barcode Detector supported!');
+    }else{
+      alert('Barcode Detector is not supported by this browser, using the Dynamsoft Barcode Reader polyfill.');
+      
+      BarcodeDetectorPolyfill.setLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==");
+      let reader = await BarcodeDetectorPolyfill.init();
+      console.log(reader); // You can modify the runtime settings of the reader instance.
+      window.BarcodeDetector = BarcodeDetectorPolyfill;
+    }
+    
+    barcodeDetector = new window.BarcodeDetector();
+    
+    document.getElementById("status").innerHTML = "";
+  }
+
+  function loadDevicesAndPlay(){
+    var constraints = {video: true, audio: false};
+    navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+        localStream = stream;
+        var cameraselect = document.getElementById("cameraSelect");
+        cameraselect.innerHTML="";
+        navigator.mediaDevices.enumerateDevices().then(function(devices) {
+            var count = 0;
+            var cameraDevices = [];
+            var defaultIndex = 0;
+            for (var i=0;i<devices.length;i++){
+                var device = devices[i];
+                if (device.kind == 'videoinput'){
+                    cameraDevices.push(device);
+                    var label = device.label || `Camera ${count++}`;
+                    cameraselect.add(new Option(label,device.deviceId));
+                    if (label.toLowerCase().indexOf("back") != -1) {
+                      defaultIndex = cameraDevices.length - 1;
+                    }
+                }
+            }
+
+            if (cameraDevices.length>0) {
+              cameraselect.selectedIndex = defaultIndex;
+              play(cameraDevices[defaultIndex].deviceId);
+            }else{
+              alert("No camera detected.");
+            }
+        });
+
+    });
+  }
+
+  function play(deviceId, HDUnsupported) {
+    stop();
+    var constraints = {};
+
+    if (!!deviceId){
+        constraints = {
+            video: {deviceId: deviceId},
+            audio: false
+        }
+    }else{
+        constraints = {
+            video: true,
+            audio: false
+        }
+    }
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        localStream = stream;
+        var camera = document.getElementsByClassName("camera")[0];
+        // Attach local stream to video element
+        camera.srcObject = stream;
+
+    }).catch(function(err) {
+        console.error('getUserMediaError', err, err.stack);
+        alert(err.message);
+    });
+  }
+
+  function stop(){
+    clearInterval(interval);
+    try{
+        if (localStream){
+            localStream.getTracks().forEach(track => track.stop());
+        }
+    } catch (e){
+        alert(e.message);
+    }
+  }
+
+  function onCameraChanged(){
+    var cameraselect = document.getElementById("cameraSelect");
+    var deviceId = cameraselect.selectedOptions[0].value;
+    play(deviceId);
+  }
+
+  function onPlayed() {
+    updateSVGViewBoxBasedOnVideoSize();
+    startDecoding(); 
+  }
+
+  function updateSVGViewBoxBasedOnVideoSize(){
+    var camera = document.getElementsByClassName("camera")[0];
+    var svg = document.getElementsByTagName("svg")[0];
+    svg.setAttribute("viewBox","0 0 "+camera.videoWidth+" "+camera.videoHeight);
+  }
+
+  function startDecoding(){
+    clearInterval(interval);
+    //1000/25=40
+    interval = setInterval(decode, 40);
+  }
+
+  async function decode(){
+    if (decoding === false) {
+      console.log("decoding");
+      var video = document.getElementsByClassName("camera")[0];
+      decoding = true;
+      var barcodes = await barcodeDetector.detect(video);
+      decoding = false;
+      console.log(barcodes);
+      if (barcodes.length != 0) {
+        let UPCcode = barcodes;
+        stop();
+        // let detectedCode = UPCcode[0].rawValue;
+        let formattedDetected;
+        if (UPCcode[0].format === "upc_e") {
+          if(UPCcode[0].rawValue.substring(6,7) === "0" || UPCcode[0].rawValue.substring(6,7) === "1" || UPCcode[0].rawValue.substring(6,7) === "2"){
+            formattedDetected = UPCcode[0].rawValue.substring(1, 3) + UPCcode[0].rawValue.substring(6, 7) + "0000" + UPCcode[0].rawValue.substring(3, 6);
+          }
+          if(UPCcode[0].rawValue.substring(6,7) === "3"){
+            formattedDetected = UPCcode[0].rawValue.substring(1, 4) + "00000" + UPCcode[0].rawValue.substring(4, 6);
+          }
+          if(UPCcode[0].rawValue.substring(6,7) === "4"){
+            formattedDetected = UPCcode[0].rawValue.substring(1, 5) + "00000" + UPCcode[0].rawValue.substring(5, 6);
+          }
+          if(UPCcode[0].rawValue.substring(6,7) === "5" || UPCcode[0].rawValue.substring(6,7) === "6" || UPCcode[0].rawValue.substring(6,7) === "7" || UPCcode[0].rawValue.substring(6,7) === "8" || UPCcode[0].rawValue.substring(6,7) === "9"){
+            formattedDetected = UPCcode[0].rawValue.substring(1, 6) + "0000" + UPCcode[0].rawValue.substring(6, 7);
+          }
+        }
+        else if (UPCcode[0].format === "ean_13"){ 
+          formattedDetected = UPCcode[0].rawValue;
+        }
+        console.log(formattedDetected);
+        console.log("WE HAVE A RESPONSE: " + UPCcode[0].rawValue);
+        $.get(
+          "/searchManualUPC",
+          {UPCsubmitted : formattedDetected},
+         // if we get a response from the server .... 
+          function(response) {
+            
+            console.log("Mission was aborted:" + response[0]);
+            if (response[0]) {
+              let error = "Sorry, couldn't find anything. Try again."
+              console.log(error);
+              displayErrorMessage(error);
+            }
+            else {
+              console.log("we have a response");
+              displayItem(response[1]);
+              displayCompany(response[2]);
+            }
+      })
+      }
+      // drawOverlay(barcodes);
+    }
+  }
+
+  // function drawOverlay(barcodes){
+  //   var svg = document.getElementsByTagName("svg")[0];
+  //   svg.innerHTML = "";
+  //   for (var i=0;i<barcodes.length;i++) {
+  //     var barcode = barcodes[i];
+  //     console.log(barcode);
+  //     var lr = {};
+  //     lr.x1 = barcode.cornerPoints[0].x;
+  //     lr.x2 = barcode.cornerPoints[1].x;
+  //     lr.x3 = barcode.cornerPoints[2].x;
+  //     lr.x4 = barcode.cornerPoints[3].x;
+  //     lr.y1 = barcode.cornerPoints[0].y;
+  //     lr.y2 = barcode.cornerPoints[1].y;
+  //     lr.y3 = barcode.cornerPoints[2].y;
+  //     lr.y4 = barcode.cornerPoints[3].y;
+  //     var points = getPointsData(lr);
+  //     var polygon = document.createElementNS("http://www.w3.org/2000/svg","polygon");
+  //     polygon.setAttribute("points",points);
+  //     polygon.setAttribute("class","barcode-polygon");
+  //     var text = document.createElementNS("http://www.w3.org/2000/svg","text");
+  //     text.innerHTML = barcode.rawValue;
+  //     text.setAttribute("x",lr.x1);
+  //     text.setAttribute("y",lr.y1);
+  //     text.setAttribute("fill","red");
+  //     text.setAttribute("fontSize","20");
+  //     svg.append(polygon);
+  //     svg.append(text);
+  //   }
+  // }
+
+  function getPointsData(lr){
+    var pointsData = lr.x1+","+lr.y1 + " ";
+    pointsData = pointsData+ lr.x2+","+lr.y2 + " ";
+    pointsData = pointsData+ lr.x3+","+lr.y3 + " ";
+    pointsData = pointsData+ lr.x4+","+lr.y4;
+    return pointsData;
+  }
+
+
+  //GET DATA BY KEYWORD
+  document.querySelector("#findDataWithKeyword").addEventListener('click', function(event){
+    console.log("click");
+    let keyword = document.getElementById("searchCritKeyword").value;
+    console.log(keyword);
+    $.get(
+      "/searchManualKeyword",
+      {keywordSubmitted : keyword},
+    // if we get a response from the server .... 
+      function(response) {
+        console.log("Mission was aborted:" + response[0]);
+        if (response[0]) {
+          let error = "Sorry, couldn't find anything. Try again."
+          console.log(error);
+          displayErrorMessage(error);
+        }
+        else {
+          displayItem(response[1]);
+          displayCompany(response[2]);
+        }
+    })
+  });//click
 
   //GET DATA BY UPC CODE
   document.querySelector("#findDataWithUPC").addEventListener('click', function(event){
